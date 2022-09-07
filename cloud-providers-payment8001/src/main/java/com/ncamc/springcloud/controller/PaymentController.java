2f93d6bb016f99976e3f82f2f177c31f9bcf7fdb
hgq2002 <h15629526027@163.com>
2022-09-07 15:12:33 +0800

    Add discovery service

--- cloud-providers-payment8001/src/main/java/com/ncamc/springcloud/controller/PaymentController.java
+++ cloud-providers-payment8001/src/main/java/com/ncamc/springcloud/controller/PaymentController.java
@@ -3,18 +3,27 @@ package com.ncamc.springcloud.controller;
 import com.ncamc.springcloud.entities.CommonResult;
 import com.ncamc.springcloud.entities.Payment;
 import com.ncamc.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Author: hugaoqiang
  * @CreateTime: 2022-09-01 11:07
  */
+@Slf4j
 @RestController
 public class PaymentController
 {
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment)
@@ -27,4 +36,19 @@ public class PaymentController
     {
         return paymentService.getPaymentById(id);
     }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery()
+    {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
 }
